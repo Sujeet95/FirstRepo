426d5d5c5019b14715b3ad37eab6e8b1680b178c
Sujeet2658 <sujeetkumar778205@gmail.com>
2024-02-03 22:41:07 +0530

    first time adding

---
+++ discount.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 22:12:12 2024
+
+@author: sujee
+"""
+
+print("Hii, How are you?")
+print("Welcome to my shop")
+n=input("Enter the items cost: ")
+print("Is this item cost is: ",n)
+d=float(n)
+ans=0.9 * d;
+print("The cost after discount is: ",ans);
---
+++ multiplication.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 23:02:25 2024
+
+@author: sujee
+"""
+"""
+n=input("What tabel would you like to display: ")
+t=int(n)
+for i in range(1,11):
+    print(t,"X",i,"=",t*i)
+    """
+    
+    
+    =input("Enter the number")
+    i=1
+    while i<=10:
+        
+    print(num, "X", i, "=", num*i)
+    i+=1
